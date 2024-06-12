48280abcd62f0c12ce20d79b5b6f7806c361b9a3
Lucio Baiocchi <lucio.baiocchi@studio.unibo.it>
2024-06-12 11:28:30 +0200

    Added client.py and server.py

---
+++ client.py
@@ -0,0 +1,67 @@
+import socket
+import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+class ChatClient:
+    def __init__(self, host, port):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((host, port))
+        
+        self.root = tkinter.Tk()
+        self.root.title("Chat Room")
+        
+        self.chat_box = tkinter.scrolledtext.ScrolledText(self.root)
+        self.chat_box.pack(padx=20, pady=5)
+        self.chat_box.config(state=tkinter.DISABLED)
+        
+        self.input_area = tkinter.Text(self.root, height=3)
+        self.input_area.pack(padx=20, pady=5)
+        
+        self.send_button = tkinter.Button(self.root, text="Send", command=self.send_message)
+        self.send_button.pack(padx=20, pady=5)
+        
+        self.alias = simpledialog.askstring("Nickname", "Please choose a nickname", parent=self.root)
+        if self.alias:
+            self.client.send(self.alias.encode('utf-8'))
+        
+        self.receive_thread = threading.Thread(target=self.receive_messages)
+        self.receive_thread.start()
+        
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.root.mainloop()
+    
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.client.send(self.alias.encode('utf-8'))
+                else:
+                    self.chat_box.config(state=tkinter.NORMAL)
+                    self.chat_box.insert(tkinter.END, message + '\n')
+                    self.chat_box.config(state=tkinter.DISABLED)
+                    self.chat_box.yview(tkinter.END)
+            except:
+                print('An error occurred!')
+                self.client.close()
+                break
+    
+    def send_message(self):
+        message = f'{self.alias}: {self.input_area.get("1.0", tkinter.END)}'
+        self.client.send(message.encode('utf-8'))
+        self.chat_box.config(state=tkinter.NORMAL)
+        self.chat_box.insert(tkinter.END, message + '\n')
+        self.chat_box.config(state=tkinter.DISABLED)
+        self.chat_box.yview(tkinter.END)
+        self.input_area.delete('1.0', tkinter.END)
+    
+    def on_closing(self):
+        self.client.close()
+        self.root.destroy()
+
+if __name__ == "__main__":
+    host = '127.0.0.1'
+    port = 8080
+    ChatClient(host, port)
---
+++ server.py
@@ -0,0 +1,54 @@
+import socket
+import threading
+
+# Lista dei client connessi
+clients = []
+aliases = []
+
+# Funzione per gestire i messaggi ricevuti da un client
+def handle_client(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message, client)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            alias = aliases[index]
+            broadcast(f'{alias} has left the chat!'.encode('utf-8'), client)
+            aliases.remove(alias)
+            break
+
+# Funzione per inviare un messaggio a tutti i client
+def broadcast(message, client):
+    for c in clients:
+        if c != client:
+            c.send(message)
+
+# Funzione principale per iniziare il server
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('127.0.0.1', 8080))
+    server.listen()
+
+    print('Server is listening...')
+
+    while True:
+        client, address = server.accept()
+        print(f'Connection from {address}')
+
+        client.send('NICK'.encode('utf-8'))
+        alias = client.recv(1024).decode('utf-8')
+        aliases.append(alias)
+        clients.append(client)
+
+        print(f'Nickname of the client is {alias}')
+        broadcast(f'{alias} has joined the chat!'.encode('utf-8'), client)
+        client.send('You are now connected!'.encode('utf-8'))
+
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+if __name__ == "__main__":
+    main()
